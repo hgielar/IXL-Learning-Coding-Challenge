cdfe2bd23870ac8f0095424dc1f6da2a606c09f3
Raleigh Littles <raleighllittles@gmail.com>
2019-09-07 16:54:21 -0700

    restructured repo to match common template. Ready to test CI

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # About
 
-After someone on Reddit posted the questions from [IXL Learning](https://www.ixl.com/)'s coding challenge, I decided to give it a go.
+Annotated solutions to [IXL](https://www.ixl.com/)'s coding challenge.
+
+# About
+
+Questions are located in `questions`, code is located in `src`, and `test` contains the tests.
--- src/q3_grid_game.py
+++ src/q3_grid_game.py
@@ -1,19 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Apr 26 20:29:08 2018
-
-@author: raleigh-littles
-"""
-
-import unittest
-
 def function(steps, n):
     """
-    The trick here is to realize that the cells with the highest value are those 
+    The trick here is to realize that the cells with the highest value are those
     that are incremented the most amount of times -- but, since the lower bounds
     start at (0, 0), note that there is at least one cell being incremented at every
     step. This then means that the cell that is incremented every time is the one
-    that has the highest value; to find these cells, we take the union across all 
+    that has the highest value; to find these cells, we take the union across all
     the rows/columns that are incremented at each time, the union will tell us which
     row/column got incremented each time, and thus which is the maximum value.
     """
@@ -23,23 +14,13 @@ def function(steps, n):
         row_bound, col_bound = map(int, step.split(' '))
         row_range = list(range(0, row_bound))
         col_range = list(range(0, col_bound))
-        
+
         row_sets.append(set(row_range))
         col_sets.append(set(col_range))
-        
+
     row_intersection = set.intersection(*row_sets)
     col_intersection = set.intersection(*col_sets)
-    
+
     cells = len(row_intersection) * len(col_intersection)
-    
+
     return cells
-
-
-class TestGridGame(unittest.TestCase):
-    def test(self):
-        self.assertEqual( function(['2 3', '3 7', '4 1'], 3), 2)
-
-if __name__ == '__main__':
-    unittest.main()
-        
-        
